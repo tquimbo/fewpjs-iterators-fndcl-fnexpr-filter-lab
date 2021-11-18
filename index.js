c0f183aa4709a0e8919fbd72a2d2521d7fa70b3a
Tim Quimbo <tquimbo1@gmail.com>
2021-11-17 21:57:15 -0500

    commit tree 6fe422c301af54f4eda8f99370efc7717ae91b5d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(arr, string) {
+    return arr.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  }
+  
+  function fuzzyMatch(arr, string) {
+    return arr.filter(driver => driver.startsWith(string) === string.startsWith(string));
+  } 
+  
+  function matchName(arr, string) {
+    return arr.filter(driver => driver.name === string);
+  }
